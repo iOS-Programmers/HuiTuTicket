db4f366d8117c6ab7416f4cb1498c193beb2ad02
jerry1230 <Bird@BirddeMacBook-Pro.local>
2014-07-22 17:48:55 +0800

    首页的ui

---
+++ HuiTuTicket/Classes/Controllers/Home/HTHomeHeadView.h
@@ -0,0 +1,13 @@
+//
+//  HTHomeHeadView.h
+//  HuiTuTicket
+//
+//  Created by Bird on 14-7-22.
+//  Copyright (c) 2014年 HuiTuTicket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTHomeHeadView : UIView
+
+@end
---
+++ HuiTuTicket/Classes/Controllers/Home/HTHomeHeadView.m
@@ -0,0 +1,31 @@
+//
+//  HTHomeHeadView.m
+//  HuiTuTicket
+//
+//  Created by Bird on 14-7-22.
+//  Copyright (c) 2014年 HuiTuTicket. All rights reserved.
+//
+
+#import "HTHomeHeadView.h"
+
+@implementation HTHomeHeadView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
