ea40ef0e6c11829f97279d56224e27c602f17266
李 广军 <823974987@qq.com>
2014-08-07 17:34:30 +0800

    订单填写界面

---
+++ HuiTuTicket/Classes/Controllers/Home/HTOrderWriteViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTOrderWriteViewController.h
+//  HuiTuTicket
+//
+//  Created by 李 广军 on 14-8-7.
+//  Copyright (c) 2014年 HuiTuTicket. All rights reserved.
+//
+
+#import "HTBaseTableViewController.h"
+
+@interface HTOrderWriteViewController : HTBaseTableViewController
+
+@end
