4db518e783dd6bcff4d0c2b92ef54bd7c52cef30
jerry1230 <Bird@BirddeMacBook-Pro.local>
2014-09-10 21:01:01 +0800

    增加支付回调处理
    增加支付回调处理

--- HuiTuTicket/HTAppDelegate.m
+++ HuiTuTicket/HTAppDelegate.m
@@ -17,7 +17,9 @@
 #import "HTMineViewController.h"
 #import "HTMoreViewController.h"
 
-
+#import "AlixPayResult.h"
+#import "DataVerifier.h"
+#import "PartnerConfig.h"
 @implementation HTAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -125,6 +127,70 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+//独立客户端回调函数
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+	
+	[self parse:url application:application];
+	return YES;
+}
+
+- (void)parse:(NSURL *)url application:(UIApplication *)application
+{
+    //结果处理
+    AlixPayResult* result = [self handleOpenURL:url];
+    
+	if (result)
+    {
+		
+		if (result.statusCode == 9000)
+        {
+			/*
+			 *用公钥验证签名 严格验证请使用result.resultString与result.signString验签
+			 */
+            
+            //交易成功
+            //            NSString* key = @"签约帐户后获取到的支付宝公钥";
+            //			id<DataVerifier> verifier;
+            //            verifier = CreateRSADataVerifier(key);
+            //
+            //			if ([verifier verifyString:result.resultString withSign:result.signString])
+            //            {
+            //                //验证签名成功，交易结果无篡改
+            //			}
+            
+        }
+        else
+        {
+            //交易失败
+        }
+    }
+    else
+    {
+        //失败
+    }
+}
+
+- (AlixPayResult *)resultFromURL:(NSURL *)url
+{
+	NSString * query = [[url query] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+#if ! __has_feature(objc_arc)
+    return [[[AlixPayResult alloc] initWithString:query] autorelease];
+#else
+	return [[AlixPayResult alloc] initWithString:query];
+#endif
+}
+
+- (AlixPayResult *)handleOpenURL:(NSURL *)url
+{
+	AlixPayResult * result = nil;
+    
+	if (url != nil && [[url host] compare:@"safepay"] == 0) {
+		result = [self resultFromURL:url];
+	}
+    
+	return result;
 }
 
 @end
