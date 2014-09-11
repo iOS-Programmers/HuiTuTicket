a098a0befa86d7e4ab4de6c27f83b99cb4fcf971
Johnny <iyhjiang@gmail.com>
2014-09-11 16:19:06 +0800

    增加景区门票订单列表页

---
+++ HuiTuTicket/Classes/Controllers/Orders/HTScenicOrderListCell.m
@@ -0,0 +1,40 @@
+//
+//  HTScenicOrderListCell.m
+//  HuiTuTicket
+//
+//  Created by Chemayi on 14/9/11.
+//  Copyright (c) 2014年 HuiTuTicket. All rights reserved.
+//
+
+#import "HTScenicOrderListCell.h"
+#import "TicketOrderModel.h"
+
+@implementation HTScenicOrderListCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)configureWithData:(TicketOrderModel *)info
+{
+    if ([info isKindOfClass:[TicketOrderModel class]]) {
+        self.scenicName.text = info.scenicName;
+        self.tourTime.text = [NSString stringWithFormat:@"出游日期：%@",info.traveltime];
+
+        self.orderTime.text = [NSString stringWithFormat:@"下单日期：%@",info.addtime];
+        
+        self.price.text = [NSString stringWithFormat:@"￥%@",info.totalamount];
+    }
+    
+    
+}
+
+@end
