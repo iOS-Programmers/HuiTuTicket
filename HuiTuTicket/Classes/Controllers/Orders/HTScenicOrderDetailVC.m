74288e8fb1633be97cedcea7c17034c67867c96c
jerry1230 <Iceland@IcelanddeMacBook-Pro.local>
2014-09-12 00:27:35 +0800

    add HTScenicOrderDetailVC
    add HTScenicOrderDetailVC

---
+++ HuiTuTicket/Classes/Controllers/Orders/HTScenicOrderDetailVC.m
@@ -0,0 +1,38 @@
+//
+//  HTScenicOrderDetailVC.m
+//  HuiTuTicket
+//
+//  Created by Iceland on 14-9-12.
+//  Copyright (c) 2014年 HuiTuTicket. All rights reserved.
+//
+
+#import "HTScenicOrderDetailVC.h"
+
+@interface HTScenicOrderDetailVC ()
+
+@end
+
+@implementation HTScenicOrderDetailVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
