d4126a0a9a8e2172c9f5cff028795e045ff8102f
jerry1230 <Bird@BirddeMacBook-Pro-3.local>
2014-10-21 22:37:59 +0800

    套票详情

--- HuiTuTicket/Classes/Controllers/Home/HTTaoPiaoYuDingController.m
+++ HuiTuTicket/Classes/Controllers/Home/HTTaoPiaoYuDingController.m
@@ -15,6 +15,7 @@
 #import "TaoPiaoScenicInfo.h"
 
 #import "TaoPiaoTicket.h"
+#define DATEPICKER_HEIGHT 216
 
 @interface HTTaoPiaoYuDingController ()
 
@@ -31,14 +32,18 @@
 @property (weak, nonatomic) IBOutlet UITextField *userNameTF;
 @property (weak, nonatomic) IBOutlet UITextField *phoneTF;
 
+@property (nonatomic,strong)UIDatePicker *datePicker;
+@property (nonatomic,strong)UIToolbar *toolBar;
+@property (nonatomic,copy)NSString *selDate;
+@property (nonatomic,copy)NSString *currID;
 
 @property (strong, nonatomic) TPOrderSubmitHttp *submitHttp;
+
+@property (nonatomic,strong)NSMutableDictionary *dataDic;
 
 - (IBAction)onPayBtnClick:(id)sender;
 - (IBAction)onPlusAction:(UIButton *)sender;
 - (IBAction)onMinAction:(UIButton *)sender;
-
-
 
 @end
 
@@ -51,6 +56,7 @@
         // Custom initialization
         self.title = @"填写订单";
         _submitHttp = [[TPOrderSubmitHttp alloc] init];
+        _dataDic = [[NSMutableDictionary alloc] init];
     }
     return self;
 }
@@ -62,11 +68,26 @@
     self.tableView.rowHeight = 88;
     self.tableView.tableHeaderView = self.tableHeaderView;
     self.tableView.tableFooterView = self.tableFootView;
+    [self.tableView frameSetHeight:[HTFoundationCommon getScreenHeight] + 20];
+    self.tableView.clipsToBounds = YES;
     
-    [self.tableView frameSetHeight:[HTFoundationCommon getScreenHeight] + (IOS7_OR_LATER ? 20 : 0) - 44];
-    
-    
+    [self dateSelectorInit];
     [self updateUIWithInfo];
+    [self shuchuViewFrame];
+}
+
+- (void)shuchuViewFrame
+{
+    for (int i=0; i<[self.view.subviews count]; i++)
+    {
+        UIView *aview = [self.view.subviews objectAtIndex:i];
+        NSLog(@"%@:%@",[aview class],NSStringFromCGRect(aview.frame));
+        for (int j=0; j<[aview.subviews count]; j++)
+        {
+            UIView *bview = [aview.subviews objectAtIndex:j];
+            NSLog(@"%@:%@",[bview class],NSStringFromCGRect(bview.frame));
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -75,6 +96,184 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSString *)getTime
+{
+    NSString *str = @"";
+    for (int i = 0; i<[[self.dataDic allKeys] count]; i++)
+    {
+        NSString *key = [[self.dataDic allKeys] objectAtIndex:i];
+        if (i==0)
+        {
+            str = [NSString stringWithFormat:@"%@|%@",key,[self.dataDic objectForKey:key]];
+        }
+        else
+        {
+            str = [NSString stringWithFormat:@"%@,%@|%@",str,key,[self.dataDic objectForKey:key]];
+        }
+    }
+    return str;
+}
+
+- (NSString *)getScenicid
+{
+    NSString *str = @"";
+    for (int i = 0; i<[self.productInfo.scenicinfo count]; i++)
+    {
+        TaoPiaoScenicInfo *info = [[self.dataDic allKeys] objectAtIndex:i];
+        if (i==0)
+        {
+            str = [NSString stringWithFormat:@"%@",info.scenicId];
+        }
+        else
+        {
+            str = [NSString stringWithFormat:@"%@,%@",str,info.scenicId];
+        }
+    }
+    return str;
+}
+
+- (NSString *)getTicketId
+{
+    NSString *str = @"";
+    for (int i = 0; i<[self.productInfo.scenicinfo count]; i++)
+    {
+        TaoPiaoScenicInfo *info = [[self.dataDic allKeys] objectAtIndex:i];
+        for (int j=0; j<[info.ticket count]; j++)
+        {
+            TaoPiaoTicket *ticket= info.ticket[j];
+            if ([str length]==0)
+            {
+                str = [NSString stringWithFormat:@"%@",ticket.ticketId];
+            }
+            else
+            {
+                str = [NSString stringWithFormat:@"%@,%@",str,ticket.ticketId];
+            }
+        }
+    }
+    return str;
+}
+
+- (void)dateSelectorInit
+{
+    self.datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height + 44, 320, DATEPICKER_HEIGHT)];
+    
+    if (IOS7_OR_LATER) {
+        [_datePicker setBackgroundColor:kColorWhite];
+    }
+    
+    [_datePicker setTimeZone:[NSTimeZone systemTimeZone]];
+    [_datePicker setDate:[NSDate date] animated:YES];
+    [_datePicker setMinimumDate:[NSDate date]];
+    [_datePicker setDatePickerMode:UIDatePickerModeDate];
+    [_datePicker addTarget:self action:@selector(datePickerValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:_datePicker];
+    
+    _toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, [LXUtils GetScreeHeight]-44, [LXUtils GetScreeWidth], 44)];
+    _toolBar.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin;
+    LXLog(@"SEE %@",NSStringFromCGRect(self.view.frame));
+    
+    NSMutableArray *buttons = [[NSMutableArray alloc] init];
+    UIBarButtonItem *myDoneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                                  target:self
+                                                                                  action:@selector(dateFinished)];
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    [buttons addObject:flexibleSpace];
+    [buttons addObject: myDoneButton];
+    [self.toolBar setItems:buttons animated:TRUE];
+    [self.view addSubview:_toolBar];
+}
+
+- (void)showDateSelector
+{
+    [self.view bringSubviewToFront:self.toolBar];
+    [self.view bringSubviewToFront:self.datePicker];
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDuration:0.3];
+    self.toolBar.frame = CGRectMake(0, self.view.frame.size.height - DATEPICKER_HEIGHT-44, 320, 44);
+    self.datePicker.frame = CGRectMake(0, self.view.frame.size.height - DATEPICKER_HEIGHT, 320, DATEPICKER_HEIGHT);
+    [UIView commitAnimations];
+}
+
+- (void)datePickerValueChanged:(id)sender
+{
+    NSDate *select = [self.datePicker date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    NSString *selectedDate = [dateFormatter stringFromDate:select];
+    self.selDate =selectedDate;
+}
+
+- (void)dateFinished
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    NSString *selectedDate = [dateFormatter stringFromDate:[NSDate date]];
+    [self.dataDic setObject:FBIsEmpty(self.selDate)?selectedDate:self.selDate forKey:self.currID];
+    [self hideDatePicker];
+    [self.tableView reloadData];
+}
+
+-(void)hideDatePicker
+{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:0.3];
+    self.toolBar.frame = CGRectMake(0, self.view.frame.size.height, 320, 44);
+    self.datePicker.frame = CGRectMake(0, self.view.frame.size.height+44, 320, DATEPICKER_HEIGHT);
+    [UIView commitAnimations];
+}
+
+- (void)submitOrder
+{
+//    校验串生成方法：将请求参数中的uid、scenicid、ticketid、number、travel_time、receive_moblie、key七个参数的value值拼成一个无间隔的字符串(顺序不要改变)，key是参数签名密钥。
+//    MD5字符串示例：
+//    106201307011536259678253012013-07-1613800138000key
+
+
+    if ([[self.dataDic allKeys] count]==[self.productInfo.scenicinfo count])
+    {
+        self.submitHttp.parameter.travel_time = [self getTime];
+
+        self.submitHttp.parameter.tpid = self.productInfo.tpid;
+        self.submitHttp.parameter.number = self.ticketNumLabel.text;
+        self.submitHttp.parameter.receive_name = self.userNameTF.text;
+        self.submitHttp.parameter.receive_moblie = self.phoneTF.text;
+        
+         NSString *strUnMd5= [NSString stringWithFormat:@"%@%@%@%@%@%@%@",[[HTUserInfoManager shareInfoManager] userId],[self getScenicid],[self getTicketId],self.ticketNumLabel.text,[self getTime],self.phoneTF.text,API_KEY];
+        self.submitHttp.parameter.sig = [HTFoundationCommon md5:strUnMd5];
+        [self showLoadingWithText:kLOADING_TEXT];
+        __weak HTTaoPiaoYuDingController *weak_self = self;
+        [self.submitHttp getDataWithCompletionBlock:^{
+            [weak_self hideLoading];
+            if (weak_self.submitHttp.isValid) {
+            
+                [weak_self showSuccess];
+                
+                
+            }
+            else {
+                //显示服务端返回的错误提示
+                [weak_self showErrorWithText:weak_self.submitHttp.erorMessage];
+            };
+        }failedBlock:^{
+            [weak_self hideLoading];
+            if (![HTFoundationCommon networkDetect]) {
+                
+                [weak_self showErrorWithText:kNETWORK_ERROR];
+            }
+            else {
+                
+                //统统归纳为服务器出错
+                [weak_self showErrorWithText:kSERVICE_ERROR];
+            };
+        }];
+    }
+    else
+    {
+        [self showErrorWithText:@"请选择出行日期"];
+    }
+}
 
 #pragma mark - Action
 
@@ -87,20 +286,35 @@
             self.dataSource = self.productInfo.scenicinfo;
             [self.tableView reloadData];
         }
-        
     }
 }
 
 - (IBAction)onPayBtnClick:(id)sender {
+    [self submitOrder];
 }
 
-- (IBAction)onPlusAction:(UIButton *)sender {
+- (IBAction)onPlusAction:(UIButton *)sender
+{
+    int count = [self.ticketNumLabel.text intValue];
+    self.ticketNumLabel.text = [NSString stringWithFormat:@"%d",count+1];
 }
 
-- (IBAction)onMinAction:(UIButton *)sender {
+- (IBAction)onMinAction:(UIButton *)sender
+{
+    int count = [self.ticketNumLabel.text intValue];
+    if (count!=0)
+    {
+        self.ticketNumLabel.text = [NSString stringWithFormat:@"%d",count-1];
+    }
 }
 
-
+#pragma mark - UITableView Delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    TaoPiaoScenicInfo *info = (TaoPiaoScenicInfo *)self.dataSource[indexPath.row];
+    self.currID = info.scenicId;
+    [self showDateSelector];
+}
 #pragma mark - UITableView DataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -126,6 +340,12 @@
         TaoPiaoTicket *ticket = (TaoPiaoTicket *)info.ticket[0];
         cell.ticketNumLabel.text = [NSString stringWithFormat:@"%@张",ticket.num];
         cell.ticketTypeLabel.text = [NSString stringWithFormat:@"%@",ticket.ticketName];
+        NSString *str = [self.dataDic objectForKey:info.scenicId];
+        if (FBIsEmpty(str))
+        {
+            str = @"请选择出行日期";
+        }
+        [cell.travelDateBtn setTitle:str forState:UIControlStateNormal];
     }
     
     cell.scenicName.text = info.scenicName;
